7a8aeaf72fd31a0afb95036d07ff0f46a50f9b34
jawojtow <jawojtow@studen.wszib.edu.pl>
2023-11-04 15:16:27 +0100

    1 zmiany

--- src/MyName.java
+++ src/MyName.java
@@ -1,6 +1,6 @@
 public class MyName {
     public static void main(String[] args) {
-        String name = "Kuba";
+        String name = "KubAAAA";
 
         System.out.println("Cześć, mam na imię " + name + ".");
     }
