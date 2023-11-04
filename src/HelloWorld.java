420e5350fd9a116303a7aa56cef66566ac959c70
wiktorkrzy <wkrzywdz@student.wszib.edu.pl>
2023-11-04 15:04:03 +0100

    Revert "Dodano pętle do Hellworld"
    This reverts commit 6f3751bdc2a8a5750485fa137fe48d5adc565ee6.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-
-        for(int i = 0; i < 5; i++){
-            System.out.println("Hello World!");
-        }
-
+        System.out.println("Hello World!");
     }
-
 }
