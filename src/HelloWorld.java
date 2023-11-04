1fed8e8ce97f2a1c56b036dfcceea2b53e13cee0
makuback <kolorowy1245@gmail.com>
2023-11-04 15:08:46 +0100

    Dodaj działanie matematyczne.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a = 4;
+        int b = 5;
+        System.out.println("Ile to jest 4 + 5? " + (a+b));
     }
 }
