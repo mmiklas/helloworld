af6e457acd3279d0752e209a9ba81ba13edf8bd5
Kubuś <szwaj.jakub@gmail.com>
2023-11-04 15:10:17 +0100

    upperCasehello

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        String hello = "Hello Wold!";
         for (int i = 5; i > 0; i--) {
 
-            System.out.println("Hello World!");
+            System.out.println(hello.toUpperCase());
         }
     }
 }
