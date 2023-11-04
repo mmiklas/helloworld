35818dafff52a615e4c2edcceb8b6dc4b1c8ebc7
gadamski <gadamski@student.wszib.edu.pl>
2023-11-04 09:17:24 +0100

    Dodanie nowych danych

--- src/MyName.java
+++ src/MyName.java
@@ -1,8 +1,12 @@
 public class MyName {
     public static void main(String[] args) {
         String name = "Marcin";
+        String surname = "Maciejczak";
+        int age = 5;
 
         System.out.println("Cześć, mam na imię " + name + ".");
+        System.out.println("Na nazwisko " + surname + ".");
+        System.out.println("I mam " + age + ".");
     }
 }
 
