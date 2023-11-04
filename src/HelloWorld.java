4a30aba7fac8ab67a8fec5a09e53f97dd329b834
eslota <ewas4852@gmail.com>
2023-11-04 15:12:06 +0100

    Revert "Skopiowano x2."
    This reverts commit 788988e85a6b6c9487cf5b57582a37dcfa92edd2.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!" + "nice to meet you" + (2 +3));
-        System.out.println("Hello World!" + "nice to meet you" + (2 +3));
     }
 
 }
