3b3594cfb3685dc4a05132aee43b2dc99153a2b0
Kubuś <szwaj.jakub@gmail.com>
2023-11-04 15:02:27 +0100

    new-feature1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+
+        }
         System.out.println("Hello World!");
     }
 }
--- src/MyName.java
+++ src/MyName.java
@@ -1,6 +1,6 @@
 public class MyName {
     public static void main(String[] args) {
-        String name = "Marcin";
+        String name = "Jakub";
 
         System.out.println("Cześć, mam na imię " + name + ".");
     }
