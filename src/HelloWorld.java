e1e38277dc8ce86988d1173a834a176450b144d7
mdomarad <michalunoo@gmail.com>
2023-11-04 15:13:17 +0100

    Revert "Dodanie autorskiej zmiany numer 2."
    This reverts commit 12e77a2b12133c019f133fcc1ffc0bfc70311cd9.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,6 @@ public class HelloWorld {
 
         System.out.println("Hello World!");
         byte a = 1;
-        byte b = 2;
-
         System.out.println("Autorska zmiana numer: " + a);
-        System.out.println("Autorska zmiana numer: " + b);
     }
 }
