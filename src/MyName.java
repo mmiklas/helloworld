3611e631c28597f7d3e770b8bdd01f617771c0dd
gadamski <gadamski@student.wszib.edu.pl>
2023-11-04 09:19:50 +0100

    Revert "Dodanie zmiennej informującej o stanie zdrowia"
    This reverts commit 088184f90290fd95f143204d42dddfb37efde232.

--- src/MyName.java
+++ src/MyName.java
@@ -3,12 +3,10 @@ public class MyName {
         String name = "Marcin";
         String surname = "Maciejczak";
         int age = 5;
-        boolean jestChory = true;
 
         System.out.println("Cześć, mam na imię " + name + ".");
         System.out.println("Na nazwisko " + surname + ".");
         System.out.println("I mam " + age + ".");
-
     }
 }
 
