4b7f92762ca400061fc1fc95d84a8ded66617ad2
gadamski <gadamski@student.wszib.edu.pl>
2023-11-04 09:19:54 +0100

    Revert "Dodanie nowych danych"
    This reverts commit 35818dafff52a615e4c2edcceb8b6dc4b1c8ebc7.

--- src/MyName.java
+++ src/MyName.java
@@ -1,12 +1,8 @@
 public class MyName {
     public static void main(String[] args) {
         String name = "Marcin";
-        String surname = "Maciejczak";
-        int age = 5;
 
         System.out.println("Cześć, mam na imię " + name + ".");
-        System.out.println("Na nazwisko " + surname + ".");
-        System.out.println("I mam " + age + ".");
     }
 }
 
