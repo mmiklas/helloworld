088184f90290fd95f143204d42dddfb37efde232
gadamski <gadamski@student.wszib.edu.pl>
2023-11-04 09:18:23 +0100

    Dodanie zmiennej informującej o stanie zdrowia

--- src/MyName.java
+++ src/MyName.java
@@ -3,10 +3,12 @@ public class MyName {
         String name = "Marcin";
         String surname = "Maciejczak";
         int age = 5;
+        boolean jestChory = true;
 
         System.out.println("Cześć, mam na imię " + name + ".");
         System.out.println("Na nazwisko " + surname + ".");
         System.out.println("I mam " + age + ".");
+
     }
 }
 
