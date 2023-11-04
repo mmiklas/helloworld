3d83095cdbc13995678883c907de93340f3695e6
mdomarad <michalunoo@gmail.com>
2023-11-04 15:11:04 +0100

    Dodanie autorskiej zmiany numer 1.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        byte a = 1;
+        System.out.println("Autorska zmiana numer: " + a);
     }
 }
