bc010f13ba7013a6ded61962dca5afd2ccb5dff7
fdziala <fi.dziala@gmail.com>
2023-11-04 15:40:31 +0100

    Student upgrade

--- src/MyName.java
+++ src/MyName.java
@@ -1,8 +1,9 @@
 public class MyName {
     public static void main(String[] args) {
         String name = "Marcin";
-
+        String studentName = "Filip";
         System.out.println("Cześć, mam na imię " + name + ".");
+        System.out.println("Cześć, a ja " + studentName + ".");
     }
 }
 
