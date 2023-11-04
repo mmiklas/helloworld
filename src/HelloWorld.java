c4f0ee4c18b64e3cee1ac51dc0e889367b94a7b1
pistanik <piotr.stnk@gmail.com>
2023-11-04 15:10:30 +0100

    autorska zmiana

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+       System.out.println("Hello World!");
+       for (int i =1; i <11; i++) {
+           System.out.println("Autorska zmiana nr" + i);
+       }
     }
 }
