788988e85a6b6c9487cf5b57582a37dcfa92edd2
eslota <ewas4852@gmail.com>
2023-11-04 15:10:51 +0100

    Skopiowano x2.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!" + "nice to meet you" + (2 +3));
+        System.out.println("Hello World!" + "nice to meet you" + (2 +3));
     }
 
 }
