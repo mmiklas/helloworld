a9c8a163917f472e4246af9bc96422e3a2abb895
Filip Działa <fi.dziala@gmail.com>
2023-12-07 00:48:00 +0100

    Revert "third commit"
    This reverts commit a116012f61ff790a7df1ffa5b1ce84c96a57a72f.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello there");
         System.out.println("Hello World!");
-
+        System.out.println("Hello there");
+        System.out.println("Hello World!");
+        System.out.println("Hello there");
     }
 }
