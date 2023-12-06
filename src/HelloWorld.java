28d72aa574bc8ac10a1a58260e87a28a6a65b389
Filip Działa <fi.dziala@gmail.com>
2023-12-07 00:30:44 +0100

    First commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,9 @@ public class HelloWorld {
 
         System.out.println("Hello World!");
         System.out.println("Hello there");
+        System.out.println("Hello World!");
+        System.out.println("Hello there");
+        System.out.println("Hello World!");
+        System.out.println("Hello there");
     }
 }
